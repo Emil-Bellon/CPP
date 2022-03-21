295f34f7f6caae1edc27ea3977aeaaec86ea98f3
Emil Bellon <ebellon@z2r9p2.42lyon.fr>
2022-03-21 17:28:09 +0100

    day01 ex02

---
+++ Day01/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebellon <ebellon@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/21 17:16:43 by ebellon           #+#    #+#             */
+/*   Updated: 2022/03/21 17:25:32 by ebellon          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+	std::string		string = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &string;
+	std::string&	stringREF = string;
+
+	std::cout << "Memory address of string	: " << &string << std::endl;
+	std::cout << "Memory address of stringPTR	: " << stringPTR << std::endl;
+	std::cout << "Memory address of string	: " << &stringREF << std::endl << std::endl;
+	std::cout << "Value of string		: " << string << std::endl;
+	std::cout << "Value of stringPTR	: " << *stringPTR << std::endl;
+	std::cout << "Value of string		: " << stringREF << std::endl;
+	return (0);
+}
