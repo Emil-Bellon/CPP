2ae16cb5d4a7be4fb92827fe551e97b01d85f4e3
Emil Bellon <ebellon@z2r8p2.42lyon.fr>
2022-03-25 17:50:48 +0100

    day 01 ex02 fix tipo issues

--- Day01/ex02/main.cpp
+++ Day01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ebellon <ebellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 17:16:43 by ebellon           #+#    #+#             */
-/*   Updated: 2022/03/21 17:25:32 by ebellon          ###   ########lyon.fr   */
+/*   Updated: 2022/03/25 17:49:38 by ebellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ int main(void)
 
 	std::cout << "Memory address of string	: " << &string << std::endl;
 	std::cout << "Memory address of stringPTR	: " << stringPTR << std::endl;
-	std::cout << "Memory address of string	: " << &stringREF << std::endl << std::endl;
+	std::cout << "Memory address of stringREF	: " << &stringREF << std::endl << std::endl;
 	std::cout << "Value of string		: " << string << std::endl;
 	std::cout << "Value of stringPTR	: " << *stringPTR << std::endl;
-	std::cout << "Value of string		: " << stringREF << std::endl;
+	std::cout << "Value of stringREF	: " << stringREF << std::endl;
 	return (0);
 }
