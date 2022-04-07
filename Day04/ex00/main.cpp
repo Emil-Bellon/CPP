699e656ce5a4fb459aaecff46de0d5cdda4c0136
Emil Bellon <ebellon@z3r10p1.42lyon.fr>
2022-04-07 16:07:17 +0200

    day 04 ex01 add test to main

--- Day04/ex00/main.cpp
+++ Day04/ex00/main.cpp
@@ -7,30 +7,34 @@
 #include "wronganimal.hpp"
 #include "wrongcat.hpp"
 
-int		main( void )
+int		main(void)
 {
 	Animal*			Wut = new Animal();
 	Animal*			Rex = new Dog();
 	Animal*			Leo = new Cat();
 	WrongAnimal*	Bob = new WrongCat();
+	WrongCat*		WrongLeo = new WrongCat();
 
 	std::cout << std::endl;
 
-	std::cout << Wut->getType() << " " << std::endl;
+	std::cout << Wut->getType() << std::endl;
 	Wut->makeSound();
-	std::cout << Rex->getType() << " " << std::endl;
+	std::cout << Rex->getType() << std::endl;
 	Rex->makeSound();
-	std::cout << Leo->getType() << " " << std::endl;
+	std::cout << Leo->getType() << std::endl;
 	Leo->makeSound();
-	std::cout << Bob->getType() << " " << std::endl;
+	std::cout << Bob->getType() << std::endl;
 	Bob->makeSound();
+	std::cout << WrongLeo->getType() << std::endl;
+	WrongLeo->makeSound();
 	
 	std::cout << std::endl;
 
-	delete( Wut );
-	delete( Rex );
-	delete( Leo );
-	delete( Bob );
+	delete(Wut);
+	delete(Rex);
+	delete(Leo);
+	delete(Bob);
+	delete(WrongLeo);
 
-	return ( EXIT_SUCCESS );
+	return (0);
 }
